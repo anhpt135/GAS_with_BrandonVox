6c6b271c090ac9a09aa8f2b99457191edc63c474
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-05-28 09:07:07 +0700

    04_FixCode_Offtick_DeleteSetupInputComponent

--- MeleeCombatOnline/Source/MeleeCombatOnline/Private/Character/MCO_Character.cpp
+++ MeleeCombatOnline/Source/MeleeCombatOnline/Private/Character/MCO_Character.cpp
@@ -3,32 +3,16 @@
 
 #include "Character/MCO_Character.h"
 
-// Sets default values
 AMCO_Character::AMCO_Character()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
-// Called when the game starts or when spawned
 void AMCO_Character::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
-void AMCO_Character::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AMCO_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
