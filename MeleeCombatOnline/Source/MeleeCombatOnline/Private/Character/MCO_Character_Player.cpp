f8de2cada5628fa74d3a525f5e3e3c52452eba41
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-05-28 09:31:36 +0700

    08_TaoClassCharacterPlayer dirived tu MCO_Character

---
+++ MeleeCombatOnline/Source/MeleeCombatOnline/Private/Character/MCO_Character_Player.cpp
@@ -0,0 +1,32 @@
+﻿// @nhpt150
+
+
+#include "Character/MCO_Character_Player.h"
+
+
+// Sets default values
+AMCO_Character_Player::AMCO_Character_Player()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMCO_Character_Player::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMCO_Character_Player::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AMCO_Character_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ MeleeCombatOnline/Source/MeleeCombatOnline/Public/Character/MCO_Character_Player.h
@@ -0,0 +1,28 @@
+﻿// @nhpt150
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MCO_Character.h"
+#include "MCO_Character_Player.generated.h"
+
+UCLASS()
+class MELEECOMBATONLINE_API AMCO_Character_Player : public AMCO_Character
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMCO_Character_Player();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
